8e135750ec465e8d54374c418dc45061db77d987
Elahenajafian <81524278+Elahenajafian@users.noreply.github.com>
2021-07-12 12:33:34 +0430

    icon gets updated
    I got the current value of an attribute, using:
    Element.setAttribute(name, value);

--- src/app.js
+++ src/app.js
@@ -22,12 +22,14 @@ function formatDate(timestamp) {
 }
 
 function displayTemp(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -35,10 +37,14 @@ function displayTemp(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 let apiKey = "8d2107fd2cdb7e4922d9ae2a41298ba2";
-let city = "Quchan";
+let city = "Sydney";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemp);
