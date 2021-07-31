30be42204cf98b37736defc31d59809cfc3f63e2
Elahenajafian <81524278+Elahenajafian@users.noreply.github.com>
2021-07-31 14:25:28 +0430

    The color of the C and F will change when clicked on

--- src/app.js
+++ src/app.js
@@ -64,15 +64,12 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "8d2107fd2cdb7e4922d9ae2a41298ba2";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemp(response) {
-  console.log(response);
   let temperatureElement = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
